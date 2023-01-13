7f920f6d4294000a66da2da89cb75ea311e0a452
Luis Hernández <luxo997@gmail.com>
2023-01-12 21:03:42 -0500

    Configuracion entorno de desarrollo

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const hola = 'hola';
+import '../styles/components/app.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
+
+export default App;
